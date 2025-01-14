84e476e02db9616fe99dfbe3733f68a4d166553c
DionisiiNuzh <dionisiy04@gmail.com>
2025-01-14 20:14:04 +0000

    fix: chatbot_model change

--- mobile/lib/chatbot/chatbot_model.dart
+++ mobile/lib/chatbot/chatbot_model.dart
@@ -1,7 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:json_annotation/json_annotation.dart';
 import 'package:mental_health_app/chatbot/chat_bot.dart';
 import 'package:mental_health_app/chatbot/chat_gpt_bot.dart';
+import 'package:mental_health_app/repository/chatbot_conversation_repository.dart';
+import 'package:mental_health_app/repository/firebase_repostiories.dart';
+import 'package:mental_health_app/services/auth_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'chatbot_model.g.dart';
@@ -9,50 +15,115 @@ part 'chatbot_model.g.dart';
 @riverpod
 class ChatbotModel extends _$ChatbotModel {
   ChatBot chatBot = ChatGptBot();
+  final _conversationRepo = ChatbotConversationRepository();
 
   @override
-  Future<List<Message>> build() async {
-    return [
-      Message.withDate("Hello", true),
-      Message.withDate("Hi there", false),
-      Message.withDate("Hello", true),
-      Message.withDate("Hi there", false),
-      Message.withDate("Hello", true),
-      Message.withDate("Hi there", false),
-      Message.withDate("Hello", true),
-      Message.withDate("Hi there", false),
-      Message.withDate("Helloo", true),
-      // Message.withDate("Hi thereeeee", false),
-    ];
+  Future<ConversationModel> build() async {
+    return ConversationModel("Start a Conversation", [
+      // Message.withDate("Hello", true),
+      // Message.withDate("Hi there", false),
+      // Message.withDate("Hello", true),
+      // Message.withDate("Hi there", false),
+      // Message.withDate("Hello", true),
+      // Message.withDate("Hi there", false),
+      // Message.withDate("Hello", true),
+      // Message.withDate("Hi there", false),
+      // Message.withDate("Helloo", true),
+    ]);
   }
 
   Future<void> addUserMessage(String message) async {
-    _addMessage(Message.withDate(message, false));
-    addChatBotMessage(message);
+    await _addMessage(Message.withDate(message, false));
+    await addChatBotMessage(message);
   }
 
-  void _addMessage(Message message) async {
-    List messages = await future;
+  Future<void> _addMessage(Message message) async {
+    // add to firebase
+    // final model = await future;
+    // List<Message> messages = model.messages;
+
+    late final DocRef collectionRef;
+    late final String name;
+
+    final data = await future;
+
+    if (data.conversationRef == null) {
+      collectionRef = await _conversationRepo.createConversation(
+        firstMessage: message,
+        uid: ref.read(currentUidProvider),
+        model: "unspecifed",
+      );
+      name = message.message;
+      // await getFromConversationRef(collectionRef);
+    } else {
+      name = data.name;
+      collectionRef = data.conversationRef!;
+    }
+
+    List<Message> messages = data.messages;
     messages.add(message);
-    ref.notifyListeners();
+    state = AsyncData(data.copyWith(
+      messages: messages,
+      name: name,
+      conversationRef: collectionRef,
+    ));
+    await _conversationRepo.addMessage(
+      conversationRef: collectionRef,
+      message: message,
+    );
   }
 
-  void addChatBotMessage(String answerTo) async {
+  Future<void> getFromConversationRef(DocRef collectionRef) async {
+    QuerySnapshot<Object?> querySnap =
+        await _conversationRepo.getMessages(collectionRef);
+
+    if (querySnap.docs.isEmpty) {
+    } else {
+      final List<Message> messages = querySnap.docs
+          .map((e) => Message.fromJson(e.data()! as Map<String, dynamic>))
+          .toList();
+      state = state.whenData((data) =>
+          data.copyWith(messages: messages, conversationRef: collectionRef));
+    }
+  }
+
+  Future<void> addChatBotMessage(String answerTo) async {
     print("chat bot is initiated a reponse");
-    String response = await chatBot.respond(answerTo);
+    String response = "Hello back"; // await chatBot.respond(answerTo);
     print("Response fron inside chatbotMessage: $response");
-    _addMessage(Message.withDate(response, true));
+    await _addMessage(Message.withDate(response, true));
   }
 }
 
+class ConversationModel {
+  final String name;
+  // final DateTime created;
+  final DocRef? conversationRef;
+  final List<Message> messages;
+  ConversationModel(this.name, this.messages, {this.conversationRef});
+
+  ConversationModel copyWith(
+      {String? name, List<Message>? messages, DocRef? conversationRef}) {
+    return ConversationModel(name ?? this.name, messages ?? this.messages,
+        conversationRef: conversationRef ?? this.conversationRef);
+  }
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake)
 @immutable
 class Message {
   final String message;
-  final DateTime creationTime;
-  final bool fromChatBot;
-  const Message(this.message, this.creationTime, this.fromChatBot);
+  final DateTime timeCreated;
+  final String role;
+  bool get fromChatBot => role == "user";
+  const Message(this.message, this.timeCreated, this.role);
 
   factory Message.withDate(String text, bool fromChatBot) {
-    return Message(text, DateTime.now(), fromChatBot);
+    return Message(text, DateTime.now(), fromChatBot ? "user" : "model");
   }
+
+  Map<String, dynamic> toJson() => _$MessageToJson(this);
+
+  factory Message.fromJson(Map<String, dynamic> json) =>
+      _$MessageFromJson(json);
 }
